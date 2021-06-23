28c3c2ba34adfa1d4a71af5cde9340d388aa6530
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-06-24 01:53:55 +0300

    added decorator to load user and verify password

--- app/models.py
+++ app/models.py
@@ -23,3 +23,18 @@ class User(UserMixin,db.Model):
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
